e15df72f05246a021d892c6b55b970001eb32c00
Marcelo Soffredi <msoffredi@gmail.com>
2021-11-22 19:28:33 -0600

    Updated init lambda to check for table empty or abort

--- src/handlers/init.ts
+++ src/handlers/init.ts
@@ -16,15 +16,23 @@ export const handler = async (): Promise<void> => {
     console.log('Init function started...');
 
     if (process.env.SUPER_ADMIN_EMAIL) {
-        const user = await User.create({
-            id: randomUUID(),
-            email: process.env.SUPER_ADMIN_EMAIL,
-        });
+        const users = await User.scan().all().exec();
 
-        if (user) {
-            console.log(
-                `User with email ${process.env.SUPER_ADMIN_EMAIL} created`,
-            );
+        if (users.length <= 0) {
+            const user = await User.create({
+                id: randomUUID(),
+                email: process.env.SUPER_ADMIN_EMAIL,
+            });
+
+            if (user) {
+                console.log(
+                    `User with email ${process.env.SUPER_ADMIN_EMAIL} created`,
+                );
+            }
+        } else {
+            console.log('DDB table not empty. Super user not added');
         }
+    } else {
+        console.log('No super admin email defined in environment variable');
     }
 };
